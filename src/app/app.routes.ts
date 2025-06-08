71537852129023f178e6ae524781ef718fd1c772
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-06-08 17:59:46 -0500

    Initial project configurations, routes and installing Tailwind and DaisyUI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    // TODO: Create components for each route
+    // loadComponent:
+  },
+  {
+    path: 'menu',
+    // TODO: Create components for each route
+    // loadComponent:
+  },
+  {
+    path: 'reservations',
+    // TODO: Create components for each route
+    // loadComponent:
+  },
+  {
+    path: 'events',
+    // TODO: Create components for each route
+    // loadComponent:
+  },
+  {
+    path: 'about',
+    // TODO: Create components for each route
+    // loadComponent:
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
+];
