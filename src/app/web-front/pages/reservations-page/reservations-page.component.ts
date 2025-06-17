06cee34a966e8ddfec28213859573958aa466d6d
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-06-17 17:40:00 -0500

    Starting reservation page development

---
+++ src/app/web-front/pages/reservations-page/reservations-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservations-page',
+  imports: [],
+  templateUrl: './reservations-page.component.html',
+})
+export class ReservationsPageComponent { }
