cb05f547c715f499de998b2cb8b8d7a4b2a3f397
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-06-19 15:41:50 -0500

    Working on chef service

---
+++ src/app/web-front/services/chef.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Chef } from '@front/interfaces/chef.interface';
+
+const chefsArray: Chef[] = [
+  {
+    name: 'Isabel Rossi',
+    role: 'Chef Ejecutiva',
+    quote: 'Disfruto combinar técnica y creatividad en cada plato que creo',
+    image: '/assets/photos/chefs/chef1.webp',
+  },
+  {
+    name: 'Michael Gómez',
+    role: 'Sous Chef',
+    quote: 'Me apasiona reinventar sabores clásicos y darles un toque moderno',
+    image: '/assets/photos/chefs/chef2.webp',
+  },
+  {
+    name: 'Kenji Tanaka',
+    role: 'Chef de Estación',
+    quote:
+      'Me especializo en cocinar platos con énfasis en ingredientes locales',
+    image: '/assets/photos/chefs/chef3.webp',
+  },
+];
+
+@Injectable({ providedIn: 'root' })
+export class ChefService {
+  getChefs(): Chef[] {
+    return [...chefsArray];
+  }
+}
