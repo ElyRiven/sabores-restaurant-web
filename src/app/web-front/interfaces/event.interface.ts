5d6f10ffecd82426a58903cf869ca5989c6a90ba
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-06-19 12:31:58 -0500

    Creating date pipe and working on event interface

--- src/app/web-front/interfaces/event.interface.ts
+++ src/app/web-front/interfaces/event.interface.ts
@@ -3,6 +3,7 @@ export type Category = 'bebida' | 'comida';
 export interface Event {
   title: string;
   description: string;
+  image: string;
+  date: string; // MM-dd | 12-25
   category: Category;
-  date: string;
 }
