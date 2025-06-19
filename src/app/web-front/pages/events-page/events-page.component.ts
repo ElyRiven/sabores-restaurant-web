ade9f68a5af7bdba7af2970a7f694349e5a15319
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-06-19 11:44:25 -0500

    Created category dropdown selector for events

--- src/app/web-front/pages/events-page/events-page.component.ts
+++ src/app/web-front/pages/events-page/events-page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Category } from '@front/interfaces/event.interface';
+import { CategoryTitlePipe } from '@front/pipes/category.pipe';
 
 @Component({
   selector: 'app-events-page',
-  imports: [],
+  imports: [CategoryTitlePipe],
   templateUrl: './events-page.component.html',
 })
-export class EventsPageComponent { }
+export class EventsPageComponent {
+  public categoryList: Category[] = ['bebida', 'comida'];
+
+  public selectedCategory = signal<Category | null>(null);
+
+  onCategoryChange(category: Category) {
+    this.selectedCategory.set(category);
+  }
+}
