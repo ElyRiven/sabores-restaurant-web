94485c901e73ab170c4888431e21ab36649295e2
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-06-19 18:04:21 -0500

    Working on image carousel of the page

--- src/app/web-front/pages/about-page/about-page.component.ts
+++ src/app/web-front/pages/about-page/about-page.component.ts
@@ -1,6 +1,17 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  viewChild,
+} from '@angular/core';
 import { Chef } from '@front/interfaces/chef.interface';
 import { ChefService } from '@front/services/chef.service';
+
+import Swiper from 'swiper';
+import { Autoplay, FreeMode } from 'swiper/modules';
+
+import 'swiper/css';
 
 @Component({
   selector: 'app-about-page',
@@ -10,9 +21,54 @@ import { ChefService } from '@front/services/chef.service';
 export class AboutPageComponent implements OnInit {
   #chefService = inject(ChefService);
 
-  public chefsArray: Chef[] | null = null;
+  public chefsArray: Chef[] | undefined = undefined;
+  public aboutImages: string[] = [
+    '/assets/photos/front/misc3.webp',
+    '/assets/photos/front/misc4.webp',
+    '/assets/photos/front/misc5.webp',
+    '/assets/photos/front/misc6.webp',
+    '/assets/photos/front/misc7.webp',
+    '/assets/photos/front/misc8.webp',
+    '/assets/photos/front/misc9.webp',
+    '/assets/photos/front/misc10.webp',
+  ];
+
+  public swiper: Swiper | undefined = undefined;
+  public swiperDiv = viewChild.required<ElementRef>('swiperDiv');
 
   ngOnInit(): void {
     this.chefsArray = this.#chefService.getChefs();
+
+    this.swiperInit();
+  }
+
+  swiperInit() {
+    const swiperContainer = this.swiperDiv().nativeElement;
+    if (!swiperContainer) return;
+
+    this.swiper = new Swiper(swiperContainer, {
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 10,
+      allowTouchMove: false,
+
+      freeMode: {
+        enabled: true,
+      },
+
+      autoplay: {
+        delay: 1500,
+        waitForTransition: false,
+      },
+
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+      },
+
+      modules: [Autoplay, FreeMode],
+    });
   }
 }
