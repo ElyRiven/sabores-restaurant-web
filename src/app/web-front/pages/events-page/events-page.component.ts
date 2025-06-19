4bc32e7a254f25f6e9dba373fe8be42e7dde4b55
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-06-19 12:32:28 -0500

    Adding grid display to event page

--- src/app/web-front/pages/events-page/events-page.component.ts
+++ src/app/web-front/pages/events-page/events-page.component.ts
@@ -1,16 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { Category } from '@front/interfaces/event.interface';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Category, Event } from '@front/interfaces/event.interface';
 import { CategoryTitlePipe } from '@front/pipes/category.pipe';
+import { EventDatePipe } from '@front/pipes/event-date.pipe';
+import { EventService } from '@front/services/event.service';
 
 @Component({
   selector: 'app-events-page',
-  imports: [CategoryTitlePipe],
+  imports: [CategoryTitlePipe, EventDatePipe],
   templateUrl: './events-page.component.html',
 })
-export class EventsPageComponent {
-  public categoryList: Category[] = ['bebida', 'comida'];
+export class EventsPageComponent implements OnInit {
+  #eventService = inject(EventService);
 
+  public categoryList: Category[] = ['bebida', 'comida'];
   public selectedCategory = signal<Category | null>(null);
+  public eventsArray = signal<Event[] | null>(null);
+
+  ngOnInit(): void {
+    this.eventsArray.set(this.#eventService.getAllEvents());
+  }
 
   onCategoryChange(category: Category) {
     this.selectedCategory.set(category);
