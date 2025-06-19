b333ae7b1c05bf555a5b077c386042a894ece834
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-06-19 15:45:42 -0500

    Adding chef section to the about page

--- src/app/web-front/pages/about-page/about-page.component.ts
+++ src/app/web-front/pages/about-page/about-page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Chef } from '@front/interfaces/chef.interface';
+import { ChefService } from '@front/services/chef.service';
 
 @Component({
   selector: 'app-about-page',
   imports: [],
   templateUrl: './about-page.component.html',
 })
-export class AboutPageComponent { }
+export class AboutPageComponent implements OnInit {
+  #chefService = inject(ChefService);
+
+  public chefsArray: Chef[] | null = null;
+
+  ngOnInit(): void {
+    this.chefsArray = this.#chefService.getChefs();
+  }
+}
