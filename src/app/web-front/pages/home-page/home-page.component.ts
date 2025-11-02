7bb4d2d8b99dcee49231d308ee7505070d428ed0
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-11-02 17:20:07 -0500

    feat: add new images for menu options and update home menu section

--- src/app/web-front/pages/home-page/home-page.component.ts
+++ src/app/web-front/pages/home-page/home-page.component.ts
@@ -1,19 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  inject,
-  viewChild,
-} from '@angular/core';
-
-import Swiper from 'swiper';
-import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/effect-fade';
-import 'swiper/css/effect-coverflow';
+import { Component, inject } from '@angular/core';
 
 import { MenuService } from '@front/services/menu.service';
 import { RouterLink } from '@angular/router';
@@ -24,64 +9,8 @@ import { HeroSection } from '@front/components/hero-section/hero-section.compone
   imports: [RouterLink, HeroSection],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent implements AfterViewInit {
+export class HomePageComponent {
   #menuService = inject(MenuService);
 
   public entries = this.#menuService.getPlatesByCategory('entry');
-
-  public swiper: Swiper | undefined = undefined;
-  public swiperDiv = viewChild.required<ElementRef>('swiperDiv');
-
-  ngAfterViewInit(): void {
-    this.swiperInit();
-  }
-
-  swiperInit() {
-    const swiperContainer = this.swiperDiv().nativeElement;
-    if (!swiperContainer) return;
-
-    this.swiper = new Swiper(swiperContainer, {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-      slidesPerView: 1,
-      centeredSlides: true,
-      spaceBetween: 20,
-      grabCursor: true,
-      effect: 'coverflow',
-
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-      },
-
-      breakpoints: {
-        768: {
-          slidesPerView: 3,
-        },
-      },
-
-      modules: [Navigation, Pagination, EffectCoverflow],
-
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-      },
-
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next-custom',
-        prevEl: '.swiper-button-prev-custom',
-      },
-
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
-  }
 }
