678f326cdaace75919b818179c66973eb53cb810
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-11-20 17:31:33 -0500

    feat: scaffold date and time form and add spanish  i18n and picker configuration to hlm-date-picker component

--- src/app/web-front/interfaces/reservation.interface.ts
+++ src/app/web-front/interfaces/reservation.interface.ts
@@ -1,10 +1,25 @@
-export type ReserveSeatNumbers = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+import { FormControl } from '@angular/forms';
 
-export interface Reservation {
+import { Address } from './address.interface';
+
+export type SeatNumbers = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+
+export type PaymentType = 'cash' | 'card' | 'transfer';
+
+export interface Customer {
   name: string;
   mail: string;
-  reservationDateTime: string;
   phone: string;
-  numberOfPersons: ReserveSeatNumbers;
   comments?: string;
+}
+
+export interface ReservationForm {
+  event: FormControl<string | null>;
+  date: FormControl<string | null>;
+  time: FormControl<string | null>;
+  numberOfGuests: FormControl<SeatNumbers | null>;
+  address: FormControl<Address | null>;
+  price: FormControl<number | null>;
+  customer: FormControl<Customer | null>;
+  paymentType: FormControl<PaymentType | null>;
 }
