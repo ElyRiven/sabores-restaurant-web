656c6068c9e41b453e56595bd5be6400ab8f73b1
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-11-23 16:53:28 -0500

    feat(events): update date formatting in upcoming events section and remove unused pipes

--- src/app/web-front/pages/home-page/upcoming-events-section/upcoming-events-section.component.ts
+++ src/app/web-front/pages/home-page/upcoming-events-section/upcoming-events-section.component.ts
@@ -1,12 +1,12 @@
+import { DatePipe, TitleCasePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-import { EventDatePipe } from '@front/pipes/event-date.pipe';
 import { EventService } from '@front/services/event.service';
 
 @Component({
   selector: 'upcoming-events-section',
-  imports: [RouterLink, EventDatePipe],
+  imports: [RouterLink, DatePipe, TitleCasePipe],
   templateUrl: './upcoming-events-section.component.html',
 })
 export class UpcomingEventsSectionComponent {
