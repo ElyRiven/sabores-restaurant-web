23dbc77ec176b872d4457e2ea8aaaa8095d25873
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-11-25 13:56:56 -0500

    fix(event): update event date format to ISO for consistency

--- src/app/web-front/services/event.service.ts
+++ src/app/web-front/services/event.service.ts
@@ -11,7 +11,7 @@ const eventsArray: Event[] = [
     description:
       'Descubre el arte del vino en una cata guiada por expertos, donde cada copa se acompaña de maridajes gourmet. Una experiencia ideal para quienes buscan explorar sabores con sofisticación.',
     image: 'assets/photos/events/wine-taste-event.webp',
-    date: '03-20-2025',
+    date: '2025-03-20',
     time: '4:30 PM',
     categories: ['coctel', 'degustación'],
     address: addressService.getAddressById(1),
@@ -21,7 +21,7 @@ const eventsArray: Event[] = [
     description:
       'Celebra con nosotros y viaja con el paladar a través de una cena temática con platos emblemáticos de distintas culturas, preparados con técnica gourmet para celebrar nuestro aniversario anual.',
     image: 'assets/photos/events/noche-sabores.webp',
-    date: '04-25-2025',
+    date: '2025-04-25',
     time: '8:00 PM',
     categories: ['aniversario', 'gourmet', 'coctel'],
     address: addressService.getAddressById(2),
@@ -31,7 +31,7 @@ const eventsArray: Event[] = [
     description:
       'Celebra la magia de la Navidad con una cena especial, donde sabores tradicionales se reinventan con un toque gourmet. Una velada perfecta para compartir en familia.',
     image: 'assets/photos/events/christmas-dinner.webp',
-    date: '12-25-2025',
+    date: '2025-12-25',
     time: '7:30 PM',
     categories: ['gourmet', 'invitado'],
     address: addressService.getAddressById(3),
@@ -41,7 +41,7 @@ const eventsArray: Event[] = [
     description:
       'Disfruta de un menú exclusivo de platos diseñado por nuestro chef ejecutivo, que destaca lo mejor de la temporada con técnica y presentación impecables.',
     image: 'assets/photos/events/chef-degustation.webp',
-    date: '07-12-2025',
+    date: '2025-07-12',
     time: '1:30 PM',
     categories: ['degustación', 'gala', 'gourmet'],
     address: addressService.getAddressById(1),
@@ -51,7 +51,7 @@ const eventsArray: Event[] = [
     description:
       'Relájate con cócteles y una selección de bebidas gourmet en una tarde ideal para compartir. Sabores intensos, combinaciones creativas y un ambiente distendido para disfrutar entre amigos.',
     image: 'assets/photos/events/coctail-afternoon.webp',
-    date: '10-05-2025',
+    date: '2025-10-05',
     time: '5:00 PM',
     categories: ['coctel', 'degustación'],
     address: addressService.getAddressById(3),
